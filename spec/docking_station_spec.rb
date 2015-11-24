90121ddc735997b6bad4bd01bf5eb53b6433685c
gareth4192 <gareth-123@live.com>
2015-11-24 17:31:33 +0000

    challange 11 added

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,5 +7,17 @@ it { is_expected.to respond_to :release_bike}
     expect(bike).to be_working
   end
 
+it 'docks something' do
+  bike = Bike.new
+  expect(subject.dock(bike)).to eq bike
+end
+
+it 'returns docked bikes' do
+  bike = Bike.new
+  subject.dock(bike)
+  expect(subject.dock(bike)).to eq bike
+  end
+
+
 
 end
